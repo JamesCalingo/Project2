2b75a8a6865a84c543aa85acf71499390588b554
JC <james.calingo@gmail.com>
2019-05-15 12:28:02 -0400

    push the stuff

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,5 +1,14 @@
 // Create user and add to database
 
+$("#createUser").on("click", function(event){
+  event.preventDefault();
+  const newUser = {
+    firstName: $("#firstNameInput").val().trim(),
+    lastName: $("#lastNameInput").val().trim(),
+    
+  }
+})
+
 // find and display registry
 
 // set value of product to "purchased: true"
