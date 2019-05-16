f018c5879a949688d0f23dad0212d3d88be98291
abarglowski <barglowskidrew@gmail.com>
2019-05-16 11:57:39 -0400

    saving changes

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,126 +1,104 @@
 // Create user and add to database
+$("#createUser").on("submit", function(event){
+    event.preventDefault();
 
-function Validate() {
-    console.log("got into validate");
-    var password = document.getElementById("InputPassword1").value;
-    var confirmPassword = document.getElementById("InputPassword2").value;
-    if (password != confirmPassword) {
-        alert("Passwords do not match.");
-        return false;
+    const userData = {
+      firstName: $('#firstNameInput')
+        .val()
+        .trim(),
+      lastName: $('#lastNameInput')
+        .val()
+        .trim(),
+      email: $('#emailInput')
+        .val()
+        .trim(),
+      password: $('#InputPassword1')
+        .val()
+        .trim()
+    };
+  
+    if (!userData.firstName || !userData.lastName || !userData.email || !userData.password) {
+      return swal({
+        title: "You're missing something!",
+        icon: 'error'
+      });
     }
 
-    return true;
-}
-
-$("#createUser").on("submit", function(event){
+    $.ajax({
+      url: '/api/users/register',
+      method: 'POST',
+      data: userData
+    })
+      .then(function(userData) {
+        console.log(userData);
+        return swal({
+          title: userData.message,
+          icon: 'success'
+        });
+      })
+      // .then(function() {
+      //   // custom bootstrap method
+      //   $('#createuser').tab('hide');
+      //   $('#login').tab('show');
+      // })
+      .catch(err => {
+        console.log(err);
+        return swal({
+          title: err.responseJSON.message,
+          icon: 'error'
+        });
+      });
+  })
+  
+  $("#login-btn").on("submit", function(event){
   event.preventDefault();
+  
   const userData = {
-    firstName: $('#firstNameInput')
+    email: $('#exampleInputEmail1')
       .val()
       .trim(),
-    lastName: $('#lastNameInput')
+    password: $('#exampleInputPassword1')
       .val()
-      .trim(),
-    email: $('#emailInput')
-      .val()
-      .trim(),
-    password: $('#InputPassword1')
-      .val()
-      .trim(),
-    confirmPass: $('#InputPassword2')
-       .val()
-       .trim()
+      .trim()
   };
-
-  if (!userData.firstName || !userData.lastName || !userData.email || !userData.password || !userData.confirmPass) {
+  
+  if (!userData.email || !userData.password) {
     return swal({
       title: "You're missing something!",
       icon: 'error'
     });
   }
-if(Validate()){
   $.ajax({
-    url: '/api/users/register',
+    url: '/api/users/login',
     method: 'POST',
     data: userData
   })
-    .then(function(userData) {
-      console.log(userData);
-      return swal({
-        title: userData.message,
-        icon: 'success'
-      });
+    .then(function(accessToken) {
+      console.log(accessToken);
+      localStorage.setItem('accessToken', accessToken);
+      getUserProfile();
     })
-    // .then(function() {
-    //   // custom bootstrap method
-    //   $('#createuser').tab('hide');
-    //   $('#login').tab('show');
-    // })
     .catch(err => {
       console.log(err);
       return swal({
-        title: err.responseJSON.message,
+        title: err.responseJSON.error,
         icon: 'error'
       });
     });
+  });
+  // find and display registry
+  
+  $("#findRegistry").on("click", function(event){
+    event.preventDefault();
+    var registry = $("#registryLookup").val().trim();
+    // API lookup the registry
+    if(!registry){
+      alert("We couldn't find that registry. Make sure it's spelled correctly and exactly as it was given to you, then try again.")
     }
-})
-
-$("#login-btn").on("submit", function(event){
-event.preventDefault();
-
-const userData = {
-  email: $('#exampleInputEmail1')
-    .val()
-    .trim(),
-  password: $('#exampleInputPassword1')
-    .val()
-    .trim()
-};
-
-if (!userData.email || !userData.password) {
-  return swal({
-    title: "You're missing something!",
-    icon: 'error'
-  });
-}
-
-$.ajax({
-  url: '/api/users/login',
-  method: 'POST',
-  data: userData
-})
-  .then(function(accessToken) {
-    console.log(accessToken);
-    localStorage.setItem('accessToken', accessToken);
-    getUserProfile();
-  })
-  .catch(err => {
-    console.log(err);
-    return swal({
-      title: err.responseJSON.error,
-      icon: 'error'
-    });
-  });
-});
-
-// find and display registry
-$("#findRegistry").on("click", function(event){
-  event.preventDefault();
-  var registry = $("#registryLookup").val().trim();
-  // API lookup the registry
-  if(!registry){
-    alert("We couldn't find that registry. Make sure it's spelled correctly and exactly as it was given to you, then try again.")
-  }
-    else{
-      window.location.href = "./dummy"
-    }
-  });
-
-
+      else{
+        window.location.href = "./dummy"
+      }
+    });  
   // Go to registry page - there's probably a way to make each registry its own page...
-
-// set value of product to "purchased: true"
-
-// Add product
-
+  // set value of product to "purchased: true"
+  // Add product
