aec05498f0447efe63aee99a27377707ee24e484
mebert2682 <michael.ebert2682@gmail.com>
2019-05-17 13:26:01 -0400

    users can now add products

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable":JAWSDB_URL,
+    "use_env_variable":process.env.JAWSDB_URL,
     "username": "root",
     "password": null,
     "database": "database_production",
